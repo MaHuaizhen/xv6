676a7645d7a614cecd60379c7a2a491dfd6c5d73
MAHuaizhen <always_mhz@163.com>
2021-11-08 20:00:50 +0800

    find

--- user/find.c
+++ user/find.c
@@ -1,8 +1,37 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-
-int main()
+void exec_find(char *path,char *findTar)
 {
+    int fd;
+    if((fd = open(path,0))< 0)
+    {
+        printf("file cannot open:%s\n",path);
+    }
+}
+int main(int argc, char *argv[])
+{
+    int i = 0;
+    if(argc < 2)
+    {
+        printf("error, lack of find target!\n");
+        exit(0);
+    }
+    else if(argc == 2)
+    {
+        
+        /*find at current file*/
+        exec_find(".",argv[1]);
+        exit(0);
+    }
+    else
+    {
+        for(i=2;i < argc;i++)
+        {
+            exec_find(argv[i], argv[1]);
+        }
+        exit(0);
+    }
+
     
 }
